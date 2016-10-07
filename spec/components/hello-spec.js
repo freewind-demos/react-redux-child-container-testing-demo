12cd8e7c0af3466e1b5fa0e80ec2e7856996a0a2
Peng.Li <nowind_lee@qq.com>
2016-10-07 15:18:55 +0800

    how to use a mock store to avoid that error

--- spec/components/hello-spec.js
+++ spec/components/hello-spec.js
@@ -3,18 +3,31 @@
 import React from 'react';
 import Hello from '../../src/components/Hello.jsx';
 import chai from 'chai';
+import chaiSpies from 'chai-spies';
 import chaiEnzyme from 'chai-enzyme';
 import {mount} from 'enzyme';
 import jsdomGlobal from 'jsdom-global';
+import {Provider} from 'react-redux';
 
 jsdomGlobal();
 chai.should();
 chai.use(chaiEnzyme());
+chai.use(chaiSpies());
 
 describe('<Hello />', () => {
 
   it('shows fruit names', () => {
-    const wrapper = mount(<Hello fruit={['AAA', 'BBB']}/>);
+    const mockStore = {
+      dispatch: chai.spy(),
+      getState: chai.spy(),
+      subscribe: chai.spy()
+    };
+
+    const wrapper = mount(
+      <Provider store={mockStore}>
+        <Hello fruit={['AAA', 'BBB']}/>
+      </Provider>
+    );
     wrapper.should.contain.text('AAA');
     wrapper.should.contain.text('BBB');
   });
